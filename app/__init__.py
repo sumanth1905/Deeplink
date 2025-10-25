8bc6c40d8b8a3151502e6fa7370f1248a2af6932
sumanth1905 <196187367+sumanth1905@users.noreply.github.com>
2025-10-25 14:02:40 +0530

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    password = urllib.parse.quote_plus(os.getenv('MYSQL_PASSWORD'))
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
